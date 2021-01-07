da69df0653f9d5ca3d3a5eb9b8f27153cbf82640
frontapps <frontapps@localhost.localdomain>
2021-01-07 13:00:41 +0900

    to #43

--- src/App.js
+++ src/App.js
@@ -8,11 +8,18 @@ class App extends Component {
       { name: 'Max', age: 28 },
       { name: 'Menu', age: 29 },
       { name: 'Stephanie', age: 26 }
-    ]
+    ],
+    otherState: 'some other values'
   }
 
   switchNameHandler = () => {
-    console.log('Was clicked!');
+    this.setState({
+      persons: [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Menu', age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ],
+    })
   }
 
   render() {

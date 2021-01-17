ee99c51cc478a381647b3c6808208d9e194d0fd6
Keisuke Baba <kisk0419@gmail.com>
2021-01-17 14:03:20 +0900

    to #57

--- src/App.js
+++ src/App.js
@@ -13,14 +13,10 @@ class App extends Component {
     showPersons: false
   }
 
-  switchNameHandler = (newName) => {
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Menu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ],
-    });
+  deletePersonHandler = (personIdex) => {
+    const persons = this.state.persons;
+    persons.splice(personIdex, 1);
+    this.setState({persons: persons});
   }
 
   nameChangedHandler = (event) => {
@@ -48,6 +44,22 @@ class App extends Component {
       cursor: 'pointer'
     };
 
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {
+            this.state.persons.map((person, index) => {
+              return <Person 
+                click={() => this.deletePersonHandler(index)} 
+                name={person.name} 
+                age={person.age} />
+            })
+          }
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm react app.</h1>
@@ -56,18 +68,7 @@ class App extends Component {
           style={style} 
           onClick={ this.togglePersonsHandler }
         >Toggle Persons</button>
-        { this.state.showPersons === true ?
-          <div>
-            <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-            <Person 
-              name={this.state.persons[1].name} 
-              age={this.state.persons[1].age} 
-              click={this.switchNameHandler.bind(this, 'Max!!')} 
-              changed={this.nameChangedHandler}
-            >I have a pen.</Person>
-            <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
-          </div> : false
-        }
+        {persons}
       </div>
     );
   }

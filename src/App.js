108b2a50a5fe7561440f2ecf5bce15ec03b23715
Keisuke Baba <kisk0419@gmail.com>
2021-01-24 10:59:34 +0900

    to #72

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
 import './App.css';
 import Person from './Person/Person'
+
+
+const StyledButton = styled.button`
+  background-color: ${props => props.alt ? 'red' : 'green'};
+  color: white;
+  font: inherit;
+  border: 1px solid blue;
+  padding: 8px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${props => props.alt ? 'salmon' : 'lightgreen'};
+    color: black;
+  }
+`;
 
 class App extends Component {
   state = {
@@ -12,6 +29,7 @@ class App extends Component {
     otherState: 'some other values',
     showPersons: false
   }
+
 
   deletePersonHandler = (personIndex) => {
     const persons = [...this.state.persons];
@@ -90,10 +108,10 @@ class App extends Component {
       <div className="App">
         <h1>Hi, I'm react app.</h1>
         <p className={classes.join(' ')}>This is really working!</p>
-        <button
-          style={style} 
+        <StyledButton
+          alt={this.state.showPersons}
           onClick={ this.togglePersonsHandler }
-        >Toggle Persons</button>
+        >Toggle Persons</StyledButton>
         {persons}
         </div>
     );

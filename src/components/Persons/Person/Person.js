31b044e2965eccd659ec2e05c4cc85101abbe423
Keisuke Baba <kisk0419@gmail.com>
2021-02-05 11:11:19 +0900

    to #104

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,17 +1,19 @@
 import React, {Component} from 'react';
-import classes from './Person.css';
-
+//import classes from './Person.css';
+import Aux from '../../../hoc/Auxilliary'
 
 //const person = (props) => {
 class Person extends Component {
     render() {
         console.log('[Person.js] rendering...');
         return (
-            <div className={classes.Person}>
+            //<div className={classes.Person}>
+            <Aux>
                 <p onClick={this.props.click}>I'm a {this.props.name} and I'm {this.props.age} years old!</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.name} />
-            </div>
+            </Aux>
+            //</div>
         )
     }
 }
